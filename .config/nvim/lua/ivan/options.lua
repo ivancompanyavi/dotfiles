2d665cd6710b4a9cac9b5a9e350b30aceac81fd8
Ivan Company <me@ivan-company.com>
2025-04-18 20:33:28 -0700

    improved code with Harpoon

--- .config/nvim/lua/ivan/options.lua
+++ .config/nvim/lua/ivan/options.lua
@@ -7,12 +7,15 @@ vim.opt.tabstop = 8     -- Always 8 (see :h tabstop)
 vim.opt.softtabstop = 2 -- What you expecting
 vim.opt.shiftwidth = 2  -- What you expecting
 
+vim.cmd [[colorscheme tokyonight-moon]]
+
 vim.cmd [[
   highlight Normal guibg=none
   highlight NonText guibg=none
   highlight Normal ctermbg=none
   highlight NonText ctermbg=none
 ]]
+
 
 
 vim.api.nvim_create_augroup("LspFormatting", { clear = true })
